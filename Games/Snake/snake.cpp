5531e3fde9bf119403039fa2e6f0a0b8bd17da2d
MhrDP <cumigorengbakar@yahoo.com>
2021-09-01 17:48:04 +0700

    same game and so

--- Games/Snake/snake.cpp
+++ Games/Snake/snake.cpp
@@ -1,5 +1,6 @@
 # include <iostream>
-# include <conio.h>
+# include <conio.h> // Input-output library
+// # include <windows.h>
 
 const int WIDTH = 20;
 const int HEIGHT = 20;
@@ -7,6 +8,10 @@ const int HEIGHT = 20;
 
 bool gameOver;
 int x, y, fruitX, fruitY, score;
+
+// Create an array of max 100 items
+int tailX[100], tailY[100];
+int nTail;
 
 enum eDirection{ STOP=0, LEFT, RIGHT, UP, DOWN};
 eDirection dir;
@@ -55,7 +60,20 @@ void Draw()
 			}
 			else
 			{
-				std::cout << " ";
+                bool print = false;
+                for(int k = 0; k < nTail; k++)
+                {
+                    if(tailX[k] == j && tailY[k] == i)
+                    {
+                        std::cout << "o";
+                        print = true;
+                    }
+                }
+
+                if(!print)
+                {
+                    std::cout << " ";
+                }
 			}
 		}
 		std::cout << std::endl;
@@ -67,10 +85,12 @@ void Draw()
 		std::cout << "#";
 	}
 	std::cout << std::endl;
+    std::cout << "Score: " << score << std::endl;
 }
 
 void Input()
 {
+    // Control keyboard inputs
 	if(_kbhit())
 	{
 		switch(_getch())
@@ -96,7 +116,89 @@ void Input()
 
 void Logic()
 {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
 
+    int prev2X, prev2Y;
+
+    // Append new tail to array
+    tailX[0] = x;
+    tailY[0] = y;
+    for(int i = 1; i < nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+    // Control the movements
+    switch(dir)
+    {
+        case LEFT:
+            x--;
+            break;
+        case RIGHT:
+            x++;
+            break;
+        case UP:
+            y--;
+            break;
+        case DOWN:
+            y++;
+            break;
+        default:
+            break;
+    }
+
+    // Hit the wall = game over
+    // if(x > WIDTH || x < 0 || y > HEIGHT || y < 0)
+    // {
+    //     gameOver = true;
+    // }
+    
+    // Hit the wall will teleport to the other side
+    if(x >= WIDTH)
+    {
+        x = 0;
+    }
+    else if(x < 0)
+    {
+        x = WIDTH - 1;
+    }
+
+    if(y >= HEIGHT)
+    {
+        y = 0;
+    }
+    else if(y < 0)
+    {
+        y = HEIGHT - 1;
+    }
+
+    // Game over if the snake eat its own tail
+    for(int i = 0; i < nTail; i++)
+    {
+        if(tailX[i] == x && tailY[i] == y)
+        {
+            gameOver = true;
+        }
+    }
+
+    // If the snake eat the fruits
+    if(x == fruitX && y == fruitY)
+    {
+        // Add score
+        score += 10;
+
+        // Spawn the fruit in another location
+        fruitX = rand() % WIDTH;
+        fruitY = rand() % HEIGHT;
+
+        // increase the tail length
+        nTail++;
+    }
 }
 
 int main()
@@ -107,6 +209,10 @@ int main()
 		Draw();
 		Input();
 		Logic();
+
+        // use this to make the game run slower if it a bit too fast
+        // Sleep{10} or sleep(10)
+        // Don't forget un-comment the # include <windows.h> at the top of script
 	}
 	return 0;
 }
